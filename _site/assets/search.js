4b470fb8dde02d468e53117a0a6e7631fad977db
lee-jaewook <jalee4930@gmail.com>
2022-03-04 12:49:25 +0900

    update google search

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"청춘 예찬",'url':"/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/2018/07/01/welcome.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"청춘 예찬",'url':"/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/2018/07/01/welcome.html"},{'title':"구글 검색에 노출 시키기",'url':"/2022/03/04/googleSearch.html"}]};
