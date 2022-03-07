b936aa60fbfaebcb649fb04f2ad9d3e67762ebd2
lee-jaewook <jaewook_lee@outlook.com>
2022-03-07 19:05:08 +0900

    add post

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"청춘 예찬",'url':"/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/2018/07/01/welcome.html"},{'title':"구글 검색에 노출 시키기",'url':"/2022/03/04/googleSearch.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"청춘 예찬",'url':"/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/2018/07/01/welcome.html"},{'title':"구글 검색에 노출 시키기",'url':"/2022/03/04/googleSearch.html"},{'title':"벡준 | 16003번 | 자석 장난감",'url':"/2022/03/07/bjoon_16003.html"}]};
