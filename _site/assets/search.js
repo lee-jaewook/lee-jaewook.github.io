13781fd4eabe1d6dd5cea7a81611dcf303c0e30d
lee-jaewook <jaewook_lee@outlook.com>
2022-03-12 21:24:28 +0900

    added new post

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Github(깃허브) 프로필 꾸미기(1)",'url':"/2022/03/11/git_profile_1.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"벡준 | 1157번 | 단어 공부",'url':"/2022/03/10/bjoon_1157.html"},{'title':"Github(깃허브) 프로필 꾸미기(1)",'url':"/2022/03/11/git_profile_1.html"}]};
